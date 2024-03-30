be5b5b02800e1d84ee7d7d3ea004dd861271502c
seydanur04 <143029715+seydanur04@users.noreply.github.com>
2024-03-30 15:22:58 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int vize, final, a;
+    printf("vize notunu giriniz: ");
+    scanf("%d", &vize);
+    while (vize<0 || vize>100){
+        printf("vize notunuzu hatalı girdiniz, tekrar giriniz: ");
+        scanf("%d", &vize);
+    }
+    printf("final notunu giriniz: ");
+    scanf("%d", &final); 
+    while (final<0 || final>100){
+        printf("final notunuzu hatalı girdiniz, tekrar giriniz: ");
+        scanf("%d", &final);
+    }
+    
+    a = vize*0.4 + final*0.6;
+    if(a>=60){
+        printf("geçtiniz %d",a);
+    }
+    else{
+        printf("kaldınız %d",a);
+    }
+    return 0;
+}
